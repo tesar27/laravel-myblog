e996bc22cb8b1c15db38fb31c8b92d777b2ad97f
yerbolat tazhkeyev <ytazhkeyev@gmail.com>
2019-09-16 11:20:02 +0600

    test commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function show()
+    {
+        return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        // dd(request()->all());
+        // dd(request(['title','body']));
+
+        // $post = new Post;
+        // $post->title = request("title");
+        // $post->body = request("body");
+        // $post->save();
+        // OR
+        // Post::create([
+        //     'title' => request('title'),
+        //     'body' => request("body")
+        // ]);
+        // OR
+        Post::create(request(['title','body']));
+
+        return redirect("/");
+    }
+}
