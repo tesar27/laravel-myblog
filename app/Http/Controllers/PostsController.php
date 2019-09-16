6d978f9699a135b374634779f1d8714cca5c6692
yerbolat tazhkeyev <ytazhkeyev@gmail.com>
2019-09-16 11:05:07 +0600

    complete front end change and create-post methods

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -10,5 +11,35 @@ class PostsController extends Controller
     public function index()
     {
         return view('posts.index');
+    }
+
+    public function show()
+    {
+        return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        // dd(request()->all());
+        // dd(request(['title','body']));
+
+        // $post = new Post;
+        // $post->title = request("title");
+        // $post->body = request("body");
+        // $post->save();
+        // OR
+        // Post::create([
+        //     'title' => request('title'),
+        //     'body' => request("body")
+        // ]);
+        // OR
+        Post::create(request(['title','body']));
+
+        return redirect("/");
     }
 }
